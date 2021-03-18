492df97aead29f21fcd9cb0414200f300827ffef
Oktawski <oktawian.perlowski@gmail.com>
2021-03-18 13:24:41 +0100

    Updated build.gradle and Task model

--- app/src/main/java/com/example/tah/models/Task.kt
+++ app/src/main/java/com/example/tah/models/Task.kt
@@ -1,13 +1,19 @@
 package com.example.tah.models
 
+import androidx.annotation.NonNull
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "tasks")
-data class Task(
-        @PrimaryKey val id: Int,
-        @ColumnInfo(name = "name") val name: String,
+public class Task(
+        @PrimaryKey(autoGenerate = true) val id: Int,
+
+        @ColumnInfo(name = "name")
+        @NonNull
+        val name: String,
+
         @ColumnInfo(name = "description") val description: String?,
-        @ColumnInfo(name = "is_complete") val isComplete: Boolean
+
+        @ColumnInfo(name = "is_complete") val isComplete: Boolean = false
 )
