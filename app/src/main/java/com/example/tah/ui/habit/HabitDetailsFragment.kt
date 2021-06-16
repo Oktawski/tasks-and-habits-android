b1a35b14841f2101e8638105a9097d130573c283
oktawski <oktawian.perlowski@gmail.com>
2021-06-16 15:02:59 +0200

    Habit details UI update

--- app/src/main/java/com/example/tah/ui/habit/HabitDetailsFragment.kt
+++ app/src/main/java/com/example/tah/ui/habit/HabitDetailsFragment.kt
@@ -5,10 +5,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import android.widget.AutoCompleteTextView
-import android.widget.EditText
-import android.widget.Toast
+import android.widget.*
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.tah.R
@@ -16,6 +13,8 @@ import com.example.tah.models.Habit
 import com.example.tah.utilities.ViewHabitTime
 import com.example.tah.utilities.ViewInits
 import com.example.tah.viewModels.HabitViewModel
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.textfield.TextInputLayout
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
@@ -28,6 +27,13 @@ class HabitDetailsFragment: Fragment(R.layout.details_habit), ViewInits, ViewHab
     private lateinit var description: EditText
     private lateinit var hoursInput: TextInputLayout
     private lateinit var minutesInput: TextInputLayout
+    private lateinit var startFab: FloatingActionButton
+    private lateinit var deleteEditLayout: LinearLayout
+    private lateinit var cancelSaveLayout: LinearLayout
+    private lateinit var deleteButton: MaterialButton
+    private lateinit var editButton: MaterialButton
+    private lateinit var cancelButton: MaterialButton
+    private lateinit var saveButton: MaterialButton
 
     private var habitId: Long? = -1L
 
@@ -43,8 +49,6 @@ class HabitDetailsFragment: Fragment(R.layout.details_habit), ViewInits, ViewHab
             return fragment
         }
     }
-
-    private val TAG = "DETAILS FRAGMENT"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -64,6 +68,14 @@ class HabitDetailsFragment: Fragment(R.layout.details_habit), ViewInits, ViewHab
         description = view.findViewById(R.id.description)
         hoursInput = view.findViewById(R.id.hours_layout)
         minutesInput = view.findViewById(R.id.minutes_layout)
+        startFab = view.findViewById(R.id.fab_start)
+        deleteEditLayout = view.findViewById(R.id.delete_edit_layout)
+        cancelSaveLayout = view.findViewById(R.id.cancel_save_layout)
+        deleteButton = view.findViewById(R.id.delete_button)
+        editButton = view.findViewById(R.id.edit_button)
+        cancelButton = view.findViewById(R.id.cancel_button)
+        saveButton = view.findViewById(R.id.save_button)
+
 
         return view
     }
@@ -73,8 +85,15 @@ class HabitDetailsFragment: Fragment(R.layout.details_habit), ViewInits, ViewHab
 
         getHabit()
         initSpinnerAdapters()
+        initOnClickListeners()
 
         // TODO add edit button to enable editing, default should be uneditable
+    }
+
+    override fun onResume() {
+        super.onResume()
+        deleteEditLayout.visibility = View.VISIBLE
+        cancelSaveLayout.visibility = View.GONE
     }
 
     private fun getHabit(){
@@ -86,17 +105,19 @@ class HabitDetailsFragment: Fragment(R.layout.details_habit), ViewInits, ViewHab
     }
 
     private fun inflateViews(it: Habit){
-        Log.i("NAME", "onViewCreated: ${it.name}")
         name.setText(it.name)
         description.setText(it.description)
-        var timeMap = getTimeStrings(it.sessionLength)
+        val timeMap = getTimeStrings(it.sessionLength)
         (hoursInput.editText as AutoCompleteTextView).setText(timeMap["Hours"])
         (minutesInput.editText as AutoCompleteTextView).setText(timeMap["Minutes"])
     }
 
     override fun initOnClickListeners() {
-        TODO("Not yet implemented")
+        editButton.setOnClickListener { setEditableView() }
+        cancelButton.setOnClickListener { setNotEditableView() }
+
     }
+
 
     override fun initViewModelObservables() {
         TODO("Not yet implemented")
@@ -116,5 +137,15 @@ class HabitDetailsFragment: Fragment(R.layout.details_habit), ViewInits, ViewHab
         ).also { adapter ->
             (layout.editText as AutoCompleteTextView).setAdapter(adapter)
         }
+    }
+
+    private fun setEditableView(){
+        this.deleteEditLayout.visibility = View.GONE
+        this.cancelSaveLayout.visibility = View.VISIBLE
+    }
+
+    private fun setNotEditableView(){
+        this.deleteEditLayout.visibility= View.VISIBLE
+        this.cancelSaveLayout.visibility = View.GONE
     }
 }
